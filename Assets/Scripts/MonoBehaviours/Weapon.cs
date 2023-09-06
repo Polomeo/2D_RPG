28c40b424965b8eb58b1f7d18dcb809a35881fce
Martín López <martin.g.lopez@gmail.com>
2023-09-06 16:33:52 -0300

    Empezada la implementación de Slopes para calcular el ángulo de disparo
    Para saber a dónde girar la animación al disparar.

--- Assets/Scripts/MonoBehaviours/Weapon.cs
+++ Assets/Scripts/MonoBehaviours/Weapon.cs
@@ -1,13 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Weapon : MonoBehaviour
 {
     static List<GameObject> ammoPool;
     public GameObject ammoPrefab;
     public int poolSize;
+
     public float weaponVelocity;
+
+    bool isFiring;
+
+    [HideInInspector]
+    public Animator animator;
+
+    Camera localCamera;
+
+    float positiveSlope;
+    float negativeSlope;
+
+    enum Quadrant
+    {
+        East,
+        South,
+        West,
+        North
+    }
+
 
     private void Awake()
     {
@@ -22,12 +44,51 @@ public class Weapon : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        isFiring = false;
+
+        localCamera = Camera.main;
+
+        // --- Seteamos los cuadrantes de la Slope para calcular los impactos --- //
+
+        // Puntos de las esquinas de la  pantalla
+        // (abajo a la izq = 0,0 --- arriba derecha = total ancho, total alto)
+        Vector2 lowerLeft = localCamera.ScreenToWorldPoint(new Vector2(0, 0));
+        Vector2 upperRight = localCamera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        Vector2 upperLeft = localCamera.ScreenToWorldPoint(new Vector2(0, Screen.height));
+        Vector2 lowerRight = localCamera.ScreenToWorldPoint(new Vector2(Screen.width, 0));
+
+        // Calculamos las líneas oblícuas que cruzan la pantalla
+        positiveSlope = GetSlope(lowerLeft, upperRight);
+        negativeSlope = GetSlope(upperLeft, lowerRight);
+    }
+
     private void Update()
     {
         if (Input.GetMouseButton(0))
         {
+            isFiring = true;
             FireAmmo();
         }
+
+        UpdateState();
+    }
+
+    private void UpdateState()
+    {
+        throw new NotImplementedException();
+    }
+
+    
+    // Slope es una línea oblícua
+    // Para obtenerla hace (y2 - y1) / (x2 - x1)
+    // donde x1, x2 e y1, y2 son puntos donde se choca la oblicua
+    // (Teorema de Thales)
+    float GetSlope(Vector2 pointOne, Vector2 pointTwo)
+    {
+        return (pointTwo.y - pointOne.y) / (pointTwo.x  - pointOne.x);
     }
 
     GameObject SpawnAmmo(Vector3 location)
