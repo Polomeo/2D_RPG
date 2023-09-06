84d82b105ba42c983f136ea2ca63546c35fec865
Martín López <martin.g.lopez@gmail.com>
2023-09-06 16:01:37 -0300

    Animación del player pasada a un Blend Tree
    Se usó un BlendTree para controlar las animaciones de movimiento del jugador

--- Assets/Scripts/MonoBehaviours/MovementController.cs
+++ Assets/Scripts/MonoBehaviours/MovementController.cs
@@ -7,16 +7,8 @@ public class MovementController : MonoBehaviour
     public float movementSpeed = 3.0f;
     Vector2 movement = new Vector2();
     Animator animator;
-    string animationState = "AnimationState";
     Rigidbody2D rb2d;
-    enum CharStates
-    {
-        walkEast = 1,
-        walkSouth = 2,
-        walkWest = 3,
-        walkNorth = 4,
-        idleSouth = 5
-     }
+    
 
     // Start is called before the first frame update
     void Start()
@@ -49,25 +41,16 @@ public class MovementController : MonoBehaviour
 
     void UpdateState()
     {
-        if(movement.x > 0)
+        if(Mathf.Approximately(movement.x, 0) && Mathf.Approximately(movement.y, 0))
         {
-            animator.SetInteger(animationState, (int) CharStates.walkEast);
-        }
-        else if (movement.x < 0)
-        {
-            animator.SetInteger(animationState, (int)CharStates.walkWest);
-        }
-        else if (movement.y > 0)
-        {
-            animator.SetInteger(animationState, (int)CharStates.walkNorth);
-        }
-        else if (movement.y < 0)
-        {
-            animator.SetInteger(animationState, (int)CharStates.walkSouth);
+            animator.SetBool("b_isWalking", false);
         }
         else
         {
-            animator.SetInteger(animationState, (int)CharStates.idleSouth);
+            animator.SetBool("b_isWalking", true);
         }
+
+        animator.SetFloat("f_xDir", movement.x);
+        animator.SetFloat("f_yDir", movement.y);
     }
 }
